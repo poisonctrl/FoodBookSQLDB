4348fe39e6fac9ff7f30677c14c36f8891836429
poisonctrl <belrosee@gmail.com>
2017-02-27 17:05:34 -0500

    Update SearchController.groovy

--- grails-app/controllers/foodbooksqldb/SearchController.groovy
+++ grails-app/controllers/foodbooksqldb/SearchController.groovy
@@ -1,10 +1,46 @@
 package foodbooksqldb
 
-class SearchController {
+import groovy.sql.*
+import groovy.sql.GroovyRowResult
 
-    import groovy.sql.Sql
-    import groovy.sql.GroovyRowResult
+class SearchController {
+    def username = 'dbreadonly', password = '', database = 'foodbook', server = 'localhost:1433'
+
+    def db = Sql.newInstance("jdbc:mariadb://$server/$database", username, password, 'org.mariadb.jdbc.Driver')
+
+    static responseFormats = ['json', 'xml']
+
+    def index() {
+        render ""
+    }
+
+    def myList = []
 
     def addIngredient() {
+        myList.add(params.ingredient)
+        showSearchParams()
+    }
+
+    def remIngredient() {
+        myList.remove(params.ingredient)
+        showSearchParams()
+    }
+
+    def showSearchParams() {
+        render myList
+    }
+
+    // def something() = Ingredients.find { ingredient == mylist[0] }
+
+    def theIDS = []
+
+    def search() {
+
+        db.eachRow("SELECT * FROM ingredients " +
+                "WHERE ingredient LIKE \'%" + myList[0] + "%\'"){ row ->
+                theIDS.add("$row.recipeID")
+        }
+        respond theIDS
+        theIDS.clear()
     }
 }
