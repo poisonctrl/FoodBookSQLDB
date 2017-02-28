c344f68d924db7faca461df777c2b6395ef18ccd
poisonctrl <belrosee@gmail.com>
2017-02-27 21:40:48 -0500

    Updated Query for distinct results

---
+++ grails-app/controllers/foodbooksqldb/RecipeController.groovy
@@ -0,0 +1,54 @@
+package foodbooksqldb
+
+import groovy.sql.*
+
+class RecipeController {
+
+    static responseFormats = ['json', 'xml']
+
+
+        def username = 'dbreadonly', password = '', database = 'foodbook', server = 'localhost:1433'
+        def db = Sql.newInstance("jdbc:mariadb://$server/$database", username, password, 'org.mariadb.jdbc.Driver')
+
+
+        def index() {
+            render ""
+        }
+    def recipenumber = params.recipeid
+    def recipe = []
+
+    def myrecipe() {
+        db.eachRow("SELECT * from recipe WHERE ID = " + recipenumber){ row ->
+            recipe.add("$row.recipename")
+            recipe.add("$row.recipesteps")
+            recipe.add("$row.favoritecount")
+            recipe.add("$row.recipeimagepath")
+        }
+            respond recipe
+    }
+
+    def getingrs() {
+        db.eachRow("SELECT DISTINCT ingredient from ingredients WHERE recipeid = " + recipenumber){ row ->
+            recipe.add("$row.ingredient")
+        }
+        respond recipe
+    }
+
+    def wholerecipe() {
+        myrecipe()
+        getingrs()
+        recipe.clear()
+    }
+
+    def recipeicon(recipenumber) {
+        def recipeicon = []
+
+        db.eachRow("SELECT recipename, favoritecount, recipeimagepath FROM recipe WHERE ID = " + recipenumber)
+                { row ->
+                    recipe.add("$row.recipename")
+                    recipe.add("$row.favoritecount")
+                    recipe.add("$row.recipeimagepath")
+                }
+        response recipeicon
+    }
+}
