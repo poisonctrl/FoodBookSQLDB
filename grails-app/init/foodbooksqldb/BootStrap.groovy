aca6a54f60f0efffb731502557e564b442358f07
poisonctrl <belrosee@gmail.com>
2017-03-16 11:20:22 -0400

    Authentication Working, search controller open, Faisal's Buttons Work.  Recipe is still restricted, no idea how to add a user other than bootstrap, but it is significant progress.

--- grails-app/init/foodbooksqldb/BootStrap.groovy
+++ grails-app/init/foodbooksqldb/BootStrap.groovy
@@ -1,8 +1,30 @@
 package foodbooksqldb
 
+import foodbooksqldb.*
+import grails.util.Environment
+
+
 class BootStrap {
 
     def init = { servletContext ->
+        def adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true)
+        def userRole = new Role(authority: 'ROLE_USER').save(flush: true)
+
+        switch (Environment.current) {
+            case Environment.DEVELOPMENT:
+
+                def admin = new User(username: "admin", password: "admin", enabled: true).save(flush: true)
+                UserRole.create(admin, adminRole).save()
+                UserRole.create(admin, userRole).save()
+
+                def user = new User(username: "user", password: "user", enabled: true).save(flush: true)
+                UserRole.create(user, userRole).save()
+
+                break
+            case Environment.PRODUCTION:
+                break
+        }
+
     }
     def destroy = {
     }
